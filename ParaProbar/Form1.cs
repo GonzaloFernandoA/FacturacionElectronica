ed112c87aaa1fa4893ea7be9ddb925155c2ffaa9
GonzaloFernandoA <gonzalofernandoa@gmail.com>
2015-07-17 00:31:33 -0300

    agregado de prueba

---
+++ ParaProbar/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fe.FacturacionElectronicaV2;
+using Fe.FacturacionElectronicaV2.Core.LoginWSAA;
+using System.Configuration;
+
+namespace ParaProbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FactoriaFE factory =new FactoriaFE();
+
+          ConfiguracionWS config = factory.ObtenerObjetoConfiguracion();
+
+
+
+          int cantidad = ConfigurationManager.AppSettings.Count;
+          config.RutaCertificado = ConfigurationManager.AppSettings["RutaCertificado"];
+
+          config.NombreServicio = ConfigurationManager.AppSettings["NombreServicio"];
+
+
+    //        config.Cuit = ConfigurationManager.AppSettings["Cuit"];
+           
+            
+            config.UrlLogin = System.Configuration.ConfigurationManager.AppSettings["ServidorAutorizacion"].ToString();
+            config.TiempoDeEspera = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["Timeout"]) ;
+
+            ServidorAutenticacion servidor = null;
+          
+            servidor = factory.ObtenerServidorAutenticacion(config);
+          
+            Autorizacion autoriz = servidor.ObtenerAutorizacion();
+            
+            System.Console.WriteLine(autoriz.Expiracion );
+
+        }
+    }
+}
