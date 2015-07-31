44af26f95983908527985e7d0fb9840fefe9ad7c
GonzaloFernandoA <gonzalofernandoa@gmail.com>
2015-07-31 19:50:10 -0300

    Se agrego el IVa

---
+++ ParaProbar/Iva.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaProbar
+{
+    public class Iva
+    {
+        public int Tipo { get; set; }
+        public long MontoGrabado { get; set; }
+        public long MontoIva { get; set; }
+    
+       public Iva( DataRow dr )
+        {
+            this.Tipo = Convert.ToInt32(dr.Field<string>("Tipo"));
+            this.MontoGrabado = long.Parse(dr.Field<string>("MontoGrabado").Replace(".", "")) / 100;
+            this.MontoIva = long.Parse(dr.Field<string>("MontoIva").Replace(".", "")) / 100; 
+        }
+    }
+
+}
--- ParaProbar/ManagerComprobantes.cs
+++ ParaProbar/ManagerComprobantes.cs
@@ -12,13 +12,23 @@ namespace ParaProbar
         public List<Comprobante> ObtenerComprobantes(string xml)
         {
             List<Comprobante> comprobantes = new List<Comprobante>();
-
             DataSet ds = new DataSet();
             ds.ReadXml(xml);
             IList<DataRow> dr = ds.Tables[0].AsEnumerable().ToList();
             comprobantes = dr.Select(x => new Comprobante(x)).ToList();
             return comprobantes;
-        
         }
+
+        public List<Iva> ObtenerIva(string xml)
+        {
+            List<Iva> ivas = new List<Iva>();
+  
+            DataSet ds = new DataSet();
+            ds.ReadXml(xml);
+            IList<DataRow> dr = ds.Tables[0].AsEnumerable().ToList();
+            ivas = dr.Select(x => new Iva(x)).ToList();
+            return ivas;
+        }
+
     }
 }
